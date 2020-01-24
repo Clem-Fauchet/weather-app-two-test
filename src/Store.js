a7c551f952c15e4c6a928c936690e9ed382643b9
Clem <clemence.fauchet23@gmail.com>
2020-01-24 18:25:57 +0100

    Adding bootstrap & sass

---
+++ src/Store.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export class Store extends Component {
+
+  constructor(props){
+    super(props)
+    this.state= { // Main App state
+      appName= 'Weather up'
+
+    }
+  }
+
+  
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Store
