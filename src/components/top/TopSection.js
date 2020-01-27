ea9fc8f396b3217cf20ba2aea0b7b22e73fca90c
Clem <clemence.fauchet23@gmail.com>
2020-01-27 17:49:18 +0100

    Popper Toggle for select location

--- src/components/top/TopSection.js
+++ src/components/top/TopSection.js
@@ -1,13 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './styleTop.scss'
 import Weather from './Weather'
 
+import { Manager, Reference, Popper } from 'react-popper'
+
 function Top() {
+  const [isSelectOpen, setSelectOpen] = useState(false)
+
+  const onToggleSelectLocation = (prevIsSelectOpen) => {
+    setSelectOpen((prevIsSelectOpen) => !isSelectOpen)
+  }
+
   return (
     <div className='top-container'>
       <div className='title'>Weather Up</div>
       <Weather />
-      <button className='btn btn-location'>Select location</button>
+
+      <Manager>
+        <Reference>
+          {({ ref }) => (
+            <button
+              className='btn btn-location'
+              ref={ref}
+              onClick={onToggleSelectLocation}
+            >
+              Select location
+            </button>
+          )}
+        </Reference>
+        <Popper placement='top'>
+          {({ ref, style, placement, arrowProps }) =>
+            isSelectOpen && (
+              <div ref={ref} style={style} data-placement={placement}>
+                Popper element
+                <div ref={arrowProps.ref} style={arrowProps.style} />
+              </div>
+            )
+          }
+        </Popper>
+      </Manager>
     </div>
   )
 }
