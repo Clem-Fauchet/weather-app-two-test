c3d219d67b5478fe3cda7dbe1d400368e7f8c946
Clem <clemence.fauchet23@gmail.com>
2020-01-27 17:20:21 +0100

    Loading screen with fetching

--- src/components/top/TopSection.js
+++ src/components/top/TopSection.js
@@ -1,17 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './styleTop.scss'
 import Weather from './Weather'
 
-export class Top extends Component {
-  render() {
-    return (
-      <div className='top-container'>
-        <div className='title'>Weather Up</div>
-        <Weather />
-        <button className='btn btn-location'>Select location</button>
-      </div>
-    )
-  }
+function Top() {
+  return (
+    <div className='top-container'>
+      <div className='title'>Weather Up</div>
+      <Weather />
+      <button className='btn btn-location'>Select location</button>
+    </div>
+  )
 }
 
 export default Top
--- src/components/top/Weather.js
+++ src/components/top/Weather.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import './styleTop.scss'
 
-import SunImg from '../../components/img/SunImg.svg'
-
 const api = {
   key: 'b666495dee96c140299b7af58f596c91',
   base: 'http://api.weatherstack.com/',
@@ -11,12 +9,16 @@ const api = {
 function Weather() {
   const [query, setQuery] = useState('London')
   const [weather, setWeather] = useState({})
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     fetch(`${api.base}current?access_key=${api.key}&query=${query}`)
       .then((res) => res.json())
       .then((data) => {
-        setWeather(data.current)
+        setTimeout(() => {
+          setIsLoading(false)
+          setWeather(data.current)
+        }, 1800)
       })
       .catch((err) => {
         if (err) console.error("Can't fetch API", err)
@@ -25,14 +27,20 @@ function Weather() {
 
   return (
     <div className='weather-container'>
-      <div className='header'>{query}</div>
-      <div className='inner-container'>
-        <div className='image'>
-          <img src={weather.weather_icons} alt='' />
+      {isLoading === true ? (
+        <h3 className='loading-screen'>Loading Weather...</h3>
+      ) : (
+        <div>
+          <div className='header'>{query}</div>
+          <div className='inner-container'>
+            <div className='image'>
+              <img src={weather.weather_icons} alt='Img' />
+            </div>
+            <div className='current-weather'>{weather.temperature}°</div>
+          </div>
+          <div className='footer'>{weather.weather_descriptions}</div>
         </div>
-        <div className='current-weather'>{weather.temperature}</div>
-      </div>
-      <div className='footer'>{weather.weather_descriptions}</div>
+      )}
     </div>
   )
 }
